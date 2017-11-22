ecd70121b335038ed421e81902769a3d4e7dd19d
jcarbad <jcarbad@gmail.com>
2017-11-22 00:30:23 -0600

    Numero 7 - Inicio

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-/* jslint esversion:6 */
+/* jslint esversion:6  */
 const express = require('express'),
 	  bodyParser = require('body-parser'),
 	  app = express(),
 	  port = process.env.PORT || 3001;
 
-app.use(bodyParser.urlencoded({ 
-	extended: false 
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json);
-
-app.get('/hola:name', (req, res) => {
-	res.send({
-		message: `Hola mundo, ${ res.params.names }!`
+app.get('/api/product', (req, res) => {
+	res.status(200).send({
+		products: []
 	});
 });
 
-app.listen(port, _ => console.log(`API RESTful escuchando en localhost:${ port }...`));
+app.post('/api/product', (req, res) => {
+	console.log(req.body);
+	res.status(200).send({
+		message: 'Producto recibido'
+	});
+});
 
+app.get('/api/product/:productId', (req, res) => {
+	
+});
+
+app.put('/api/product/:productId', (req, res) => {
+
+});
+
+app.delete('/api/product/:productId', (req, res) => {
+
+});
+
+app.listen(port, _ => {
+	console.log(`API RESTful en http://localhost:${ port }`);
+});
