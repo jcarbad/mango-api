adfa088321cf1f86bb2f4bb76cf655c86e2ca0f1
jcarbad <jcarbad@gmail.com>
2017-12-29 00:50:23 -0600

    Lil' comments

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /* jslint esversion:6  */
 const express = require('express'),
-	  bodyParser = require('body-parser'),
+	  bodyParser = require('body-parser'), // parses the JSON, buffer, string and URL from HTTP
+	  hbs = require('express-handlebars'), //Templating engine
 	  app = express(),
 	  api = require('./routes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.engine('.hbs'. hbs({
+	defaultLayout: 'default'
+ }));
 app.use('/api', api);
 
 module.exports = app;
